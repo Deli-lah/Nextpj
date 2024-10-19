4d8e5f72d4620a4738c3e5ef23ff297a146f95c9
HtetHtetNaingKyaw <hethtetnaingkyaw2021@gamil.com>
2024-10-19 12:00:41 +0630

    display teacher list

---
+++ app/teacher/page.tsx
@@ -0,0 +1,39 @@
+import { prisma } from "@/prisma";
+async function getAllTeachers() {
+  const teachers = await prisma.teacher.findMany({
+    include: {
+      department: true,
+    },
+  });
+  return teachers;
+}
+export default async function Teachers() {
+  const teachers = await getAllTeachers();
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>Salary</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teachers?.map((item) => (
+            <tr className=" bg-base-200" key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.gender}</td>
+              <td>{item.email}</td>
+              <td>{item.salary}</td>
+              <td>{item.address}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ component/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div
+      className=" navbar flex justify-between items-center bg-base-200"
+      style={{ padding: "15px" }}
+    >
+      <div className=" text-3xl">Cu-management</div>
+      <div className="btn btn-sm btn-secondary">Add Teacher</div>
+    </div>
+  );
+}
